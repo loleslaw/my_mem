297ce55bb8b77d33e0988131d228e83b875fdd4a
karol.pik <karol.pik@promity.pl>
2019-10-14 14:31:08 +0200

    services implemented

---
+++ lib/_models/user_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+class User {
+    final String uid;
+  final String email;
+  final String photoUrl;
+  final String displayName;
+  const User({
+    @required this.uid,
+    this.email,
+    this.photoUrl,
+    this.displayName,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'myMem',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'My mem application'),
     );
   }
 }
